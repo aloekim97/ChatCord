1d26bf552f67523bc3f885df169e2cbc5b5cea49
Remiadekunle <remiadekunle21@gmail.com>
2023-01-16 22:40:29 -0800

    added channels dropdown menu

--- react-app/src/components/ChannelIndexItem/index.js
+++ react-app/src/components/ChannelIndexItem/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux"
 import { useParams } from "react-router-dom";
 import { fetchChannels } from "../../store/channel";
@@ -10,30 +10,68 @@ function ChannelIndex(){
     const dispatch = useDispatch();
     const channelsObj = useSelector(state => state.channel)
     const {serverId} = useParams()
+    const [showMenu, setShowMenu] = useState(false);
+    const ulRef = useRef();
+
+    const openMenu = () => {
+        if (showMenu) return;
+        setShowMenu(true);
+    };
+
+
 
     useEffect(() => {
         dispatch(fetchChannels(serverId));
     }, [dispatch]);
 
+    useEffect(() => {
+        if (!showMenu) return;
+
+        const closeMenu = (e) => {
+          if (!ulRef.current.contains(e.target)) {
+            setShowMenu(false);
+          }
+        };
+
+        document.addEventListener('click', closeMenu);
+
+        return () => document.removeEventListener("click", closeMenu);
+    }, [showMenu]);
+
+    const closeMenu = () => setShowMenu(false);
+
+
     if (Object.keys(channelsObj).length < 1){
         return null
     }
+    console.log('this is state', showMenu)
 
     const channels = Object.values(channelsObj.server)
     console.log('the channels in the component',channels)
+    const ulClassName = (showMenu ? 'channel-droplist' : 'channel-droplist2');
     return(
         <div className="page-container">
             <div className="channels-container">
                 <div>
-                    Server Name
+
                 </div>
                 <div>
-                    {
-                        channels.map(channel => (
-                            <ChannelDisplay channel={channel}/>
-                        ))
-                    }
+                    Server Name
                 </div>
+                <>
+                    <button className="channel-button-container" onClick={openMenu}>
+                        TEXT CHANNELS
+                    </button>
+                    <ul className={ulClassName} ref={ulRef}>
+                        <>
+                            {
+                                channels.map(channel => (
+                                    <ChannelDisplay channel={channel}/>
+                                ))
+                            }
+                        </>
+                    </ul>
+                </>
             </div>
             <div className="messages-container">
 
