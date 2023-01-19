c9ed104b345059cf82a9dbaaca5743da2b339278
Martin Yip <martinyip220@gmail.com>
2023-01-19 15:37:50 -0800

    edit server func compelted but not styled

--- react-app/src/components/ServerIndexItem/serverDropdown.js
+++ react-app/src/components/ServerIndexItem/serverDropdown.js
@@ -6,19 +6,18 @@ import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { deleteServerThunk } from "../../store/server";
 import { useHistory } from "react-router-dom";
+import NewEditServerModal from "../newEditServerModal";
 
 function ServerDropdown({ server, channelId, channel }) {
   const dispatch = useDispatch();
   const history = useHistory();
   const user = useSelector((state) => state.session.user.id);
   const serverId = useSelector((state) => state.server.singleServer.id);
-  console.log("serverid", serverId);
-  console.log("userid", user);
 
   const serverOwner = useSelector(
     (state) => state.server.singleServer.owner.id
   );
-  console.log("serverowner id", serverOwner);
+  // console.log("serverowner id", serverOwner);
 
 //   const handleDelete = async (e) => {
 //     e.preventDefault();
@@ -30,7 +29,14 @@ function ServerDropdown({ server, channelId, channel }) {
     <div className="server-dropdown-container">
       <div style={{ width: "100%" }}>
         {user === serverOwner && (
-          <button className="server-drop-button">Edit Server</button>
+          <button className="server-drop-button">
+
+              <OpenModalMenuItem
+                itemText="Edit Server"
+                modalComponent={<NewEditServerModal serverId={server.id} />}
+              />
+
+            </button>
         )}
         <button className="server-drop-button">
           <OpenModalMenuItem
