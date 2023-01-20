87b92d83f89b94b8efdfdaa8766713eaf818c94f
Remiadekunle <remiadekunle21@gmail.com>
2023-01-19 22:48:30 -0800

    added member profile popout and did some styling

--- react-app/src/components/ServerIndexItem/index.js
+++ react-app/src/components/ServerIndexItem/index.js
@@ -50,7 +50,7 @@ function ServerPage() {
           <div className="border-break"></div>
         </div>
         <div className="serverList">
-          <div>
+          <div className="server-index-container">
             {filteredServers.map((server) => (
               <ServerIndex server={server} />
             ))}
@@ -68,7 +68,7 @@ function ServerPage() {
               />
             </button>
           </div>
-          
+
         </div>
       </div>
       {/* <div className="left-side-channels"> channels info </div> */}
